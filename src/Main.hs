a044a4d4113d042418436be73a241fcfe6ba3b79
Steven Zhang <zhzhxtrrk@gmail.com>
2015-09-29 19:54:03 +0800

    support http tunnel

--- src/Main.hs
+++ src/Main.hs
@@ -16,6 +16,11 @@ import           System.Environment
 import           System.IO
 
 
+data ProxyTransportProp = HTTP SockAddr String
+                        | TCP SockAddr
+                        deriving (Show)
+
+
 evpBytesToKey :: BS.ByteString -> Int -> Int -> (BS.ByteString, BS.ByteString)
 evpBytesToKey password keyLen ivLen =
     let ms' = BS.concat $ ms 0 []
@@ -48,22 +53,19 @@ pkcsRemovePadding block size =
         BS.take (len - fromIntegral pad) block
 
 
-talk :: Socket -> SockAddr -> BS.ByteString -> BS.ByteString -> IO ()
-talk client proxyAddr key password = do
+talk :: Socket -> ProxyTransportProp -> BS.ByteString -> BS.ByteString -> IO ()
+talk client transport key password = do
     proxy <- socket AF_INET Stream defaultProtocol
 
-    connect proxy proxyAddr
-
-    -- handshake
-    NB.sendAll proxy $ runPut $ putWord8 $ fromIntegral $ BS.length key
-    NB.sendAll proxy key
+    -- connect to the proxy
+    connectToProxy proxy transport
 
     exitSignal <- newTChanIO
 
     let (pwd, iv) = evpBytesToKey password 32 16
         cipher = initAES pwd
         final = threadFinalHandler exitSignal
-        handleProxy = recvOnProxy proxy client cipher iv
+        handleProxy = recvOnProxy transport proxy client cipher iv
         handleClient = recvOnHandle client proxy cipher iv
         in do
             -- fork receive thread - handle
@@ -76,36 +78,61 @@ talk client proxyAddr key password = do
                 _ <- readTChan exitSignal
                 return $ cleanup [proxy] [handleReceiveThread, proxyReceiveThread]
     where
-        recvOnHandle client proxy cipher iv = loop
+        recvOnHandle client proxy cipher iv = loop where
+            loop = do
+                chunk <- NB.recv client 4096
+                unless (BS.null chunk) $ do
+                    let encrypted = encryptCBC cipher iv $ pkcsPadding chunk 16
+                        len = BS.length encrypted
+                        encodedLen = runPut $ putWord32be $ fromIntegral len
+                        in NB.sendMany proxy [encodedLen, encrypted]
+                    loop
+
+        recvOnProxy transport proxy handle cipher iv = do
+            buffer <- NB.recv proxy 65536
+            loop chunkParser $ runGetPartial chunkParser buffer
             where
-                loop = do
-                    chunk <- NB.recv client 4096
-                    unless (BS.null chunk) $ do
-                        let encrypted = encryptCBC cipher iv $ pkcsPadding chunk 16
-                            len = BS.length encrypted
-                            encodedLen = runPut $ putWord32be $ fromIntegral len
-                            in NB.sendMany proxy [encodedLen, encrypted]
-                        loop
+                loop parser (Done chunk restBuffer) = do
+                    -- decrypt and forward to client
+                    let clearChunk = pkcsRemovePadding (decryptCBC cipher iv chunk) 16
+                        in NB.sendAll handle clearChunk
 
-        recvOnProxy proxy handle cipher iv =
-            let parser = do
-                    len <- getWord32be
-                    getByteString $ fromIntegral len
-                in do
+                    loop parser $ runGetPartial parser restBuffer
+                loop parser (Partial continue) = do
                     buffer <- NB.recv proxy 65536
-                    loop parser $ runGetPartial parser buffer
-                    where
-                        loop parser (Done chunk restBuffer) = do
-                            -- decrypt and forward to client
-                            let clearChunk = pkcsRemovePadding (decryptCBC cipher iv chunk) 16
-                                in NB.sendAll handle clearChunk
+                    unless (BS.null buffer) $
+                        loop parser $ continue buffer
+                loop _ (Fail err _) = error err
 
-                            loop parser $ runGetPartial parser restBuffer
-                        loop parser (Partial continue) = do
+        connectToProxy proxy transport = do
+            case transport of
+                HTTP sockAddr host -> do
+                    connect proxy sockAddr
+                    -- send http request
+                    NB.sendMany proxy [
+                        BC.pack "GET / HTTP/1.1\r\n",
+                        BC.pack $ "Host: " ++ host ++ "\r\n",
+                        BC.pack "Connection: Upgrade\r\n",
+                        BC.pack "Upgrade: DOGS\r\n",
+                        BC.pack "\r\n"]
+                    -- recv http response headers
+                    recvHeader proxy BS.empty
+                    where
+                        recvHeader proxy chunk = do
                             buffer <- NB.recv proxy 65536
                             unless (BS.null buffer) $
-                                loop parser $ continue buffer
-                        loop _ (Fail err _) = error err
+                                let newChunk = BS.concat [chunk, buffer]
+                                    in unless (BS.isSuffixOf (BC.pack "\n\n") newChunk
+                                        || BS.isSuffixOf (BC.pack "\r\n\r\n") newChunk) $
+                                        recvHeader proxy newChunk
+                TCP sockAddr -> connect proxy sockAddr
+
+            NB.sendAll proxy $ runPut $ putWord8 $ fromIntegral $ BS.length key
+            NB.sendAll proxy key
+
+        chunkParser = do
+            len <- getWord32be
+            getByteString $ fromIntegral len
 
         threadFinalHandler exitSignal e = do
             case e of
@@ -121,9 +148,17 @@ talk client proxyAddr key password = do
 
 main :: IO ()
 main = do
-    (ip:port:key:password:_) <- getArgs
+    protocol:host:port:key:password:_ <- getArgs
 
-    proxyIp <- inet_addr ip
+    transportProp <- case protocol of
+        "tcp" -> do
+            AddrInfo _ _ _ _ addr _ : _ <- getAddrInfo Nothing (Just host) (Just port)
+            return $ TCP addr
+        "http" -> do
+            AddrInfo _ _ _ _ addr _ : _ <- getAddrInfo Nothing (Just host) (Just port)
+            return $ HTTP addr host
+
+    print transportProp
 
     socket <- socket AF_INET Stream defaultProtocol
     setSocketOption socket ReuseAddr 1
@@ -136,10 +171,9 @@ main = do
 
         putStrLn $ "Hello " ++ show addr
 
-        let proxyAddr = SockAddrInet (fromIntegral (read port)) proxyIp
-            proxyKey = BC.pack key
+        let proxyKey = BC.pack key
             proxyPwd = BC.pack password
-            action = talk rSocket proxyAddr proxyKey proxyPwd
+            action = talk rSocket transportProp proxyKey proxyPwd
             final e = do
                 finalMessage <- case e of
                     Left error -> return $ "Bye " ++ show addr ++ " with error: " ++ show error
