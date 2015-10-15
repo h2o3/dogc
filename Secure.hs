a34a0df3fec227f3c0f4a1ea103138bc20efdae6
Steven Zhang <zhzhxtrrk@gmail.com>
2015-10-15 08:12:33 +0800

    format code

--- Secure.hs
+++ Secure.hs
@@ -10,34 +10,34 @@ import           Data.Monoid       ((<>))
 
 evpBytesToKey :: BS.ByteString -> Int -> Int -> (BS.ByteString, BS.ByteString)
 evpBytesToKey password keyLen ivLen =
-    let ms' = BS.concat $ ms 0 []
-        key = BS.take keyLen ms'
-        iv  = BS.take ivLen $ BS.drop keyLen ms'
-     in (key, iv)
+  let ms' = BS.concat $ ms 0 []
+      key = BS.take keyLen ms'
+      iv  = BS.take ivLen $ BS.drop keyLen ms'
+  in (key, iv)
   where
     ms :: Int -> [BS.ByteString] -> [BS.ByteString]
     ms 0 _ = ms 1 [hash password]
     ms i m
-        | BS.length (BS.concat m) < keyLen + ivLen =
-            ms (i+1) (m ++ [hash (last m <> password)])
-        | otherwise = m
+      | BS.length (BS.concat m) < keyLen + ivLen =
+        ms (i+1) (m ++ [hash (last m <> password)])
+      | otherwise = m
 
 
 pkcsPadding :: BS.ByteString -> Int -> BS.ByteString
 pkcsPadding block size =
-    let len = BS.length block
-        mlen = len `mod` size
-        pad = size - mlen
-    in
-        BS.concat [block, BS.replicate pad $ fromIntegral pad]
+  let len = BS.length block
+      mlen = len `mod` size
+      pad = size - mlen
+  in
+   BS.concat [block, BS.replicate pad $ fromIntegral pad]
 
 
 pkcsRemovePadding :: BS.ByteString -> BS.ByteString
 pkcsRemovePadding block =
-    let len = BS.length block
-        pad = BS.last block
-    in
-        BS.take (len - fromIntegral pad) block
+  let len = BS.length block
+      pad = BS.last block
+  in
+   BS.take (len - fromIntegral pad) block
 
 
 initCipher :: BS.ByteString -> (AES, BS.ByteString)
